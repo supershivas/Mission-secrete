b260dd56c9fce0fffa8811ec36dcc00f1c747231
supershivas <supershivas@users.noreply.github.com>
2026-06-12 20:20:06 +0200

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'agent-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/admin.html',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
